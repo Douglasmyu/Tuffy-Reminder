2b300daa9767a31cb5219ac9341848d0fba4641e
Douglasmyu <douglasmyu@csu.fullerton.edu>
2024-12-10 14:40:13 -0800

    app icon

---
+++ Tuffy-Reminder/Tuffy-Reminder/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Tuffy-Reminder
+//
+//  Created by Douglas Yu on 12/10/24.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
